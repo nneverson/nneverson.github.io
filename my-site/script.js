b5f8bc23bc177194a9a689983946ce06e105acd1
Neely Neverson <68759848+nneverson@users.noreply.github.com>
2021-03-21 11:57:45 -0500

    Add files via upload

---
+++ my-site/script.js
@@ -0,0 +1,7 @@
+document.getElementById ("roller").onclick = function() {myFunction(); console.log('done');};
+
+function myFunction() {
+  document.getElementById("pose").style.display = "block";
+  console.log(document.getElementById("pose"));
+}
+
